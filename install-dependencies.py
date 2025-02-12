66b694d9e0c325ea89521e63a35d09a80f06bfee
Oliver Zarate <41704658+odog96@users.noreply.github.com>
2025-02-11 21:53:32 -0500

    Update install-dependencies.py
    3rd change

--- install-dependencies.py
+++ install-dependencies.py
@@ -1,5 +1,10 @@
-# import subprocess
+import subprocess
 
-# # Install the requirements
+# Install the requirements
+print('imported sub processes')
 # subprocess.run(['pip3', 'install', '-r', 'requirements.txt'], check=True)
-!pip install --no-cache-dir -r requirements.txt
+subprocess.check_call(['pip', 'install', '--no-cache-dir', '-r', 'requirements.txt'])
+
+print('done')
+
+#!pip install --no-cache-dir -r requirements.txt
